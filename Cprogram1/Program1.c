892d8413eacabe00d878d9d423c17ccae574f8cf
Kevin <kevin.rivers14832@ku.edu>
2023-02-09 21:36:33 -0600

    testing things out

--- Cprogram1/Program1.c
+++ Cprogram1/Program1.c
@@ -2,7 +2,7 @@
  * Program1.c
  *
  *  Created on: Feb 9, 2023
- *      Author: k617r140
+ *      Author: k617r140/Kevin Riverss
  */
 
 #include <stdio.h>
---
+++ Cprogram2/Program2.c
@@ -0,0 +1,6 @@
+/*
+ * Program2.c
+ *
+ *  Created on: Feb 9, 2023
+ *      Author: k617r140/Kevin Riverss
+ */
